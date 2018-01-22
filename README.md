c088644b0b67114c3a94eafdc6b458c01f05bf2f
milka192 <35417509+milka192@users.noreply.github.com>
2018-01-22 22:48:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+symftest
+========
+
+A Symfony project created on June 28, 2017, 11:55 am.
---
+++ composer-setup.php
@@ -0,0 +1,4 @@
+
+Warning: readfile(): php_network_getaddresses: getaddrinfo failed: Nieznany host.  in Command line code on line 1
+
+Warning: readfile(https://getcomposer.org/installer): failed to open stream: php_network_getaddresses: getaddrinfo failed: Nieznany host.  in Command line code on line 1
